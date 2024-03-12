531c1f81dd1e6cbc68bf9c0bf3bfddbcdd3953f1
Juan Camilo Convers Yañez <juanconvers.legal@gmail.com>
2024-03-11 23:31:40 -0500

    Commit No. 5: Mongoose aplicado correctamente

--- src/app.js
+++ src/app.js
@@ -1,25 +1,29 @@
- 
-import express from "express";
-import productsRouter from './routes/ProductsRouter.js';
-import upload from "./config/multer.js";
-import cartRouter from "./routes/cartRouter.js";
-import { Server, Socket } from "socket.io";
+import express from 'express'
+import cartRouter from './routes/cartRouter.js'
+import productsRouter from './routes/productsRouter.js'
+import userRouter from './routes/userRoutes.js'
+// import chatRouter from './routes/chatRouter.js'
+import upload from './config/multer.js'
+import mongoose from 'mongoose'
+import { Server } from 'socket.io'
 import { engine } from 'express-handlebars'
-import { __dirname } from "./path.js";
+import { __dirname } from './path.js'
 
-
-console.log(__dirname)
-
-//configuraciones o declaraciones
+//Configuraciones o declaraciones
 const app = express()
 const PORT = 11000
 
 //Server
-const server = app.listen(PORT,() => {
+const server = app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`)
 })
 
 const io = new Server(server)
+
+//Connection DB
+mongoose.connect("mongodb+srv://juanconverslegal:Malkut27.7@cluster0.j6k2srb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => console.log("DB is connected"))
+    .catch(e => console.log(e))
 
 //Middlewares
 app.use(express.json())
@@ -27,50 +31,30 @@ app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 app.set('views', __dirname + '/views')
 
+const mensajes = []
 io.on('connection', (socket) => {
-    console.log("Conección con SOCKET.IO realizada")
+    console.log("Conexion con Socket.io")
 
-    socket.on('movimiento', info => { 
+    socket.on('mensaje', info => {
         console.log(info)
+        mensajes.push(info)
+        io.emit('mensajeLogs', mensajes)
     })
 
-    socket.on('rendirse', info => { 
-        console.log(info)
-        socket.emit('mensaje-jugador', "Te has rendido") 
-        socket.broadcast.emit('rendicion', "El jugador se ha rendido") 
-    })
 })
 
-
 //Routes
-app.use('/static', express.static(__dirname + '/public'))
+app.use('/public', express.static(__dirname + '/public'))
 app.use('/api/products', productsRouter, express.static(__dirname + '/public'))
 app.use('/api/cart', cartRouter)
+// app.use('/api/chat', chatRouter, express.static(__dirname + '/public'))
+app.use('/api/users', userRouter)
+
 app.post('/upload', upload.single('product'), (req, res) => {
     try {
         console.log(req.file)
-        res.status(200).send("Imagen subida correctamente")
+        res.status(200).send("Imagen cargada correctamente")
     } catch (e) {
-        res.status(500).send("Error al subir imagen")
+        res.status(500).send("Error al cargar imagen")
     }
-}) 
-// app.get('/static', (req, res) => {
-    
-//     const prods = [
-//         { id: 1, title: "Celular", price: 1500, img: "./img/170718249838066585_7797470128152.jpg" },
-//         { id: 2, title: "Televisor", price: 1800, img: "https://www.radiosapienza.com.ar/Image/0/500_500-526469_1.jpg" },
-//         { id: 3, title: "Tablet", price: 1200, img: "https://www.radiosapienza.com.ar/Image/0/500_500-526469_1.jpg" },
-//         { id: 4, title: "Notebook", price: 1900, img: "https://www.radiosapienza.com.ar/Image/0/500_500-526469_1.jpg" }
-//     ]
-
-    // res.render('templates/products', {
-    //     mostrarProductos: true,
-    //     productos: prods,
-    //     css: 'product.css'
-    // })
-// })
-
-
-
-
-
+})
